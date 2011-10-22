a164a16c28c7edf9d80b3a4344f7e7ebabaa5957
albertogg <albertogrespan@gmail.com>
2011-10-21 20:27:01 -0430

    Finished chapter 5 w/exercises

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
 
+	# Checks that the routing for every page are working.
 	it "should have a Home page at '/'" do
 		get '/'
 		response.should have_selector('title', :content => "Home")
@@ -24,7 +25,22 @@ describe "LayoutLinks" do
 
 	it "should have a Sign up page at '/signup'" do
 		get '/signup'
-	response.should have_selector('title', :content => "Sign up")
+		response.should have_selector('title', :content => "Sign up")
 	end
-	
+
+	# Checks for all the links on the page to be working correctly.
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		response.should have_selector('title', :content => "About")
+		click_link "Help"
+		response.should have_selector('title', :content => "Help")
+		click_link "Contact"
+		response.should have_selector('title', :content => "Contact")
+		click_link "Home"
+		response.should have_selector('title', :content => "Home")
+		click_link "Sign up now!"
+		response.should have_selector('title', :content => "Sign up")
+	end
+
 end
