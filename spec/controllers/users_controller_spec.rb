15b20a45cf3059cb1965aa57ae7ea22ae32f32f5
albertogg <albertogrespan@gmail.com>
2011-12-10 23:55:02 -0430

    Finished chapter 7

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -16,7 +16,7 @@ describe UsersController do
 
     it "should find the right user" do
       get :show, :id => @user
-      assign(:user).should == @user
+      assigns(:user).should == @user
     end
 
     it "should have the right title" do
@@ -32,6 +32,7 @@ describe UsersController do
     it "should have a profile image" do
       get :show, :id => @user
       response.should have_selector("h1>img", :class => "gravatar")
+    end
 
   end
   
@@ -46,5 +47,7 @@ describe UsersController do
       get "new"
       response.should have_selector("title", :content => "Sign up")
     end
+
   end
+
 end
